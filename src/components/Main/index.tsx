2d7ee0f4efd3daa5ee7397d4e31b2d0407dac7da
UFFETKK <soyoungan02@naver.com>
2022-01-31 03:18:18 +0900

    :기본스타일수정

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import * as S from "./style";
 
 function Main() {
+  const [word, setWord] = useState<string>("");
+  const [data, setData] = useState<any>();
+  const navigate = useNavigate();
+
+  const API_KEY = "36d5974e55f9a80fd96e9895520dafd8";
+  const submit = async (word: string) => {
+    fetch("https://dapi.kakao.com/v3/search/book?target=title&query=" + word, {
+      headers: {
+        Authorization: `KakaoAK ${API_KEY}`,
+      },
+    })
+      .then((response) => response.json())
+      .then((json) => {
+        setData(json.documents);
+      });
+    return data;
+  };
+  console.log(data);
   return (
     <S.Container>
       <div>
@@ -12,9 +31,38 @@ function Main() {
           type="text"
           placeholder="검색할 책 이름을 입력해주세요"
           maxLength={40}
+          value={word}
+          onChange={(e) => setWord(e.target.value)}
         />
-        <button type="submit">Submit</button>
+        <button
+          type="submit"
+          onClick={() => {
+            submit(word);
+          }}
+        >
+          Submit
+        </button>
       </S.Input>
+      <S.ResultContainer>
+        {data &&
+          data.map((a: any) => {
+            return (
+              <div key={a} /*onClick={() => navigate(`/detail?${a.title}`)}*/>
+                <div>
+                  <img src={a.thumbnail} alt="img" />
+                </div>
+                <div>
+                  <p>{a.title}</p>
+                  <p>{a.authors}</p>
+                </div>
+              </div>
+            );
+          })}
+      </S.ResultContainer>
+      <p>
+        마음에 드는 검색 결과를 찾지 못하셨다면 조금 더 정확한 검색어를
+        사용해주세요!
+      </p>
     </S.Container>
   );
 }
--- src/components/Main/style.ts
+++ src/components/Main/style.ts
@@ -9,9 +9,17 @@ export const Container = styled.div`
   & > div:first-child {
     font-size: 2rem;
     color: #7c7c7c;
+    font-weight: 600;
     & > span {
       color: pink;
     }
+  }
+  & > p {
+    text-align: center;
+    padding: 0.4rem;
+    background-color: rgba(255, 0, 0, 0.12);
+    border-radius: 0.5rem;
+    color: gray;
   }
 `;
 export const Input = styled.div`
@@ -29,6 +37,26 @@ export const Input = styled.div`
 
     :hover {
       color: pink;
+    }
+  }
+`;
+
+export const ResultContainer = styled.div`
+  display: flex;
+  width: inherit;
+  flex-wrap: wrap;
+  & > div {
+    padding: 1rem;
+    margin: 0.5rem;
+    flex: 1;
+    border: 1px solid #7c7c7c;
+    max-width: 10rem;
+    & > div:first-child > img {
+      width: 8rem;
+    }
+    & > div:nth-child(2) {
+      display: flex;
+      flex-direction: column;
     }
   }
 `;
